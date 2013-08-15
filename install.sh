efaf06d88345627a7e7ba15db11e4892b6f3a888
Daniel Tahara <daniel.tahara@mylookout.com>
2013-08-15 12:06:00 -0400

    Add install script; update gitignore to ignore binaries

--- install.sh
+++ install.sh
@@ -1,8 +1,10 @@
-#!/usr/bin/sh
+#!/usr/bin/zsh
 
 export SRC_ROOT=`pwd`/src
 export PG_ROOT=$SRC_ROOT/../../postgresql-9.3beta2
 
-cp -r src/document $PG_ROOT/contrib/document &&
-(cd $PG_ROOT/contrib/document; make)
+rm -rf $PG_ROOT/contrib/document
+cp -r src/postgres/document $PG_ROOT/contrib/document || exit 0
+(cd $PG_ROOT/contrib/document;
+   (cd lib/jsmn; make) && make && sudo make install)
 
